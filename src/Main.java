eeaa2341f0a6158d39b8dc1183f0186445939213
Tekindar <116880404+Tekindar@users.noreply.github.com>
2024-09-24 15:58:06 +0800

    24 Sep 2024, 3:58PM: Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import game.Gameboard;
+import player.Player;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the game");
+        Gameboard gameboard = new Gameboard();
+        Player player1 = new Player(1,"1",1,1);
+        Player player2 = new Player(2,"2",2,2);
+        gameboard.joinPlayer(player1);
+        gameboard.joinPlayer(player2);
+        gameboard.startGame();
     }
 }
